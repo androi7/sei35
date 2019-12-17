aad0ae5bdb570e5d67406468137ec8ed0ba340aa
Gabriel Hilti <gabriel.hilti@gmail.com>
2019-12-18 10:45:35 +1100

    wk2 jquery paintr

--- week2/jquery-intro/js/events.js
+++ week2/jquery-intro/js/events.js
@@ -1,43 +1,76 @@
+$(document).ready( function() {
+  $('body').append($('<p id="newP">You pressed: </p>'));
+  $('input[type=text]').on('keydown', function(e) {
+    $(e.target).css('color','gray');
+  }).on('keyup', function(e) {
+    $(e.target).css('color', 'green');
+  }).on('keypress', function(e) {
+    console.log('you pressed', String.fromCharCode(e.keyCode));
+    let content = $('#newP').html();
+    $('#newP').html(content + String.fromCharCode(e.keyCode));
+  }).on('blur', function(ev) {
+    console.log('blur', ev);
+  }).on('change', function(ev) {
+    console.log('change', ev);
+  }).on('focus', function(ev) {
+    console.log('focus', ev);
+  }).on('input', function(ev) {
+    const inputText = $(this).val();
+    //console.log(inputText);
 
-$('body').append($('<p id="newP">You pressed: </p>'));
-$('input[type=text]').on('keydown', function(e) {
-  $(e.target).css('color','gray');
-}).on('keyup', function(e) {
-  $(e.target).css('color', 'green');
-}).on('keypress', function(e) {
-  console.log('you pressed', String.fromCharCode(e.keyCode));
-  let content = $('#newP').html();
-  $('#newP').html(content + String.fromCharCode(e.keyCode));
-}).on('blur', function(ev) {
-  console.log('blur', ev);
-}).on('change', function(ev) {
-  console.log('change', ev);
-}).on('focus', function(ev) {
-  console.log('focus', ev);
-});
+    if (inputText === 'chicken' ) {
+      console.log('Chicken is the correct passoword!!');
+      $('#features').slideDown();
+    }
+  });
 
-let blurFactor = 0;
+  let blurFactor = 0;
 
-$('#kitty').on('click', function(ev) {
-  console.log(ev);
-  $(this).css({'filter': 'drop-shadow(16px 16px 20px red) invert(75%)'});
-}).on('mousemove', function(ev) {
-  blurFactor++;
-  $(this).css({'filter': `blur(${blurFactor/10}px)`});
-});
+  $('#kitty').on('click', function(ev) {
+    console.log(ev);
+    $(this).css({'filter': 'drop-shadow(16px 16px 20px red) invert(75%)'});
+  }).on('mousemove', function(ev) {
+    blurFactor++;
+    //$(this).css({'filter': `blur(${blurFactor/10}px)`});
+    $('body').css({'filter': `blur(${blurFactor/10}px)`});
+  });
 
-$('#toggle').on('click', function() {
-  $('.triple-kitty1').toggle();
-});
+  $('#toggle').on('click', function() {
+    $('.triple-kitty1').eq(0).toggle();
+  });
 
-$('#fade').on('click', function() {
-  $('.triple-kitty2').fadeToggle();
-});
+  $('#fade').on('click', function() {
+    $('#features').fadeToggle(2000);
+  });
 
-$('#show h2').on('mousedown', function() {
-  $('.triple-kitty3').show();
-});
+  $('#show h2').on('mousedown', function() {
+    $('.triple-kitty3').show();
+  });
 
-$('#show h2').on('mouseup', function() {
-  $('.triple-kitty3').hide();
+  $('#show h2').on('mouseup', function() {
+    $('.triple-kitty3').hide();
+  });
+
+
+  // Attach a click handler to every element on the page
+  // shows the propagation bubbling up structure
+  $('*').on('click', function() {
+    console.log(this);
+  });
+
+
+  $('#kitty').css({
+    position: 'relative',
+    left: 0
+  }, 3000);
+
+  $('#kitty').animate({
+      left: '100px', //window.innerWidth,
+      width: '70vw'
+    },
+    3000,
+    function() {
+      console.log('animation complete!');
+  });
+
 });
