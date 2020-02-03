9be7d36e8a4265f81b19e4bca1e08d12bf7a0cea
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-02-03 14:45:41 +1100

    Updates

---
+++ precache-manifest.788b9ab68ee910dfe8f479cc33db4794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00abe085fe53f5f2f84bcdb10d8f00ea",
+    "url": "/dentist/index.html"
+  },
+  {
+    "revision": "210941b98724e6bc748f",
+    "url": "/dentist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "35a0b29acd09447acb26",
+    "url": "/dentist/static/js/2.33231fb2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dentist/static/js/2.33231fb2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "210941b98724e6bc748f",
+    "url": "/dentist/static/js/main.43615039.chunk.js"
+  },
+  {
+    "revision": "68fc83a0ed407fb4bf10",
+    "url": "/dentist/static/js/runtime-main.1ba5c3ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dentist/static/media/logo.5d5d9eef.svg"
+  }
+]);
